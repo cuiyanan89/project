6b3072e34c79cb66aeadd2bc3ab606b1dc6bb24c
root <root@yanan-Rev-1-0.(none)>
2013-09-11 13:02:48 +0800

    work

--- work0909/future/views.py
+++ work0909/future/views.py
@@ -1,7 +1,8 @@
 # Create your views here.
 from django.shortcuts import render,render_to_response
 from django.http import HttpResponse,HttpResponseRedirect
-
+from future.models import User,Letter
+import hashlib
 
 
 
@@ -9,7 +10,15 @@ def index(req):
     return render(req,'index.html',{'user':None})
 
 def regist(req):
-    pass
+    if req.method=="POST":
+        username = req.POST.get('username',None)
+        email = req.POST.get('email',None)
+        password = req.POST.get('password',None)
+        confirm = req.POST.get('confirm',None)
+        if password == confirm:
+            password = hashlib.sha1(password).hexdigest()
+            user = User.objects.create(username=username,email=email,password=password)
+    HttpResponseRedirect('/index/')
 
 def login_view(req):
     pass
