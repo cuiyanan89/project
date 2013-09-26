89f1ce9578276a6157018f41bec605fa16c18acf
root <root@yanan-Rev-1-0.(none)>
2013-09-26 17:45:22 +0800

    a

--- work0923/blog/models.py
+++ work0923/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+#from markdown import markdown
 
 # Create your models here.
 class Node(models.Model):
@@ -10,9 +11,12 @@ class Node(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField(max_length=5000)
-#    content_html = models.TextField(max_length=10000)
+#    content_html = models.TextField(blank=True,null=True)
     create_time = models.DateField(auto_now_add = True)
     node = models.ForeignKey(Node)
 
     def __unicode__(self):
         return self.title
+
+#    def save(self):
+#        self.content_html = markdown(self.content,['codehilite'])
